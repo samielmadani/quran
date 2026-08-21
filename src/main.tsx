59acbaf44cc9a3768a88f65377d4f2b6a373b79d
Sami Elmadani <sae53@uclive.ac.nz>
2026-08-21 21:31:01 +1200

    height

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import '@ionic/react/css/core.css';
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
 import './index.css';
 import App from './App.tsx';
 import './register-sw';
